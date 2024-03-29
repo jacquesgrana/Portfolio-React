8f33232dfe9e0ae871c2ba9cec3d15f1f76cedc4
jacques <jac.granarolo.info@gmail.com>
2024-03-29 13:38:53 +0100

    ajout des polices + logo + mise en place des fichiers scss

--- src/component/template/Footer.tsx
+++ src/component/template/Footer.tsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
+import Config from "../../config/config";
 
 function Footer() {
+
+    const githubUrl = Config.GITHUB_REPO_URL;
     
     return (
         <footer id="app-footer">
-            <p>Footer</p>
+            <p className="footer-text text-center">Footer</p>
+            <p className="footer-link text-center">
+            <Link className="link-1" to={githubUrl} title="Lien vers Github">Lien vers Github</Link>
+            </p>
         </footer>
     );
 
---
+++ src/config/config.ts
@@ -0,0 +1,3 @@
+export default class Config {
+    public static GITHUB_REPO_URL = "https://github.com/jacquesgrana/Portfolio-React";
+}
