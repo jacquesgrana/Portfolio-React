ff6f073830d2ff2769ff0dec0797fb6d95fbca0e
jacques <jac.granarolo.info@gmail.com>
2024-04-01 13:24:11 +0200

    ajout mixin scss

--- src/component/template/Header.tsx
+++ src/component/template/Header.tsx
@@ -24,11 +24,11 @@ const Header = () => {
 
 
       const setActivePage = (index: number) => {
-        console.log(index);
         const linkHome = document.getElementById("link-home");
         const linkGallery = document.getElementById("link-gallery");
         const linkContact = document.getElementById("link-contact");
 
+        // TODO à améliorer
         switch (index) {
           case 0:
             linkHome?.classList.add("active");
@@ -48,12 +48,12 @@ const Header = () => {
         }
 
       }
-      //console.log(isScrolled ? "border-bottom-header-1" : "");
+
     return (
         <header id="app-header" className={isScrolled ? "border-bottom-header-1 pb-2" : "transition-03s pb-2"}>
             <h1 className="text-center title-1-bold mt-4 text-white text-space-3">PortFolio <span className="text-orange">Jacques Granarolo</span>
             </h1>
-            <Nav className="justify-content-center link-header header-nav" variant="underline" defaultActiveKey="0" >
+            <Nav className="link-header header-nav">
             <Nav.Item onClick={(e) => setActivePage(0)}>
               <Link id="link-home" to="/home" className={`link-1 text-space-3`}>
                 Accueil
