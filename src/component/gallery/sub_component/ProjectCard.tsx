a429d6c76fa79efabd06c2672dd16fa28764bb04
jacques <jac.granarolo.info@gmail.com>
2024-04-04 14:35:57 +0200

    card de la galerie modifiée

--- src/component/gallery/sub_component/ProjectCard.tsx
+++ src/component/gallery/sub_component/ProjectCard.tsx
@@ -24,25 +24,12 @@ const ProjectCard = (props: ProjectCardProps) => {
                         {props.project.subTitle}
                     </span>
                 </Card.Text>
-                <Card.Text className="mb-2 d-flex justify-content-between align-items-end">
-                    <span className="text-dark-orange-2">
-                        <strong>
-                        {format(props.project.date, 'MMMM yyyy', { locale: fr })}
-                        </strong>  
-                    </span>
-                    <span className="text-blue-5 text-size-0-75">
-                    Difficulté :&nbsp;
-                    <strong className="text-dark-orange-2">{`${props.project.difficulty}/10`}
-                    </strong>  
-                    </span>
-                </Card.Text>
                 <Stack className="mt-0 d-flex flex-wrap flex-start" direction="horizontal" gap={2}>
                     {props.project.tags.map((tag: ITag) => (
                         <Tag key={tag.id} tag={tag} />
                     ))}
                 </Stack>
                 <div className="flex-grow-1"></div>
-
              </Card.Body>
         </Card>
     );
@@ -55,4 +42,18 @@ export default ProjectCard;
                     <Button className="btn-1 bg-dark-orange" href={props.project.githubUrl} target="_blank" title="Lien vers le repository Github du projet.">Github</Button>
                     <Button className="btn-1 bg-blue-4" href={props.project.projectUrl} target="_blank" title="Lien vers le site du projet.">Projet</Button>
                 </div>
+
+
+                <Card.Text className="mb-2 d-flex justify-content-between align-items-end">
+                    <span className="text-dark-orange-2">
+                        <strong>
+                        {format(props.project.date, 'MMMM yyyy', { locale: fr })}
+                        </strong>  
+                    </span>
+                    <span className="text-blue-5 text-size-0-75">
+                    Difficulté :&nbsp;
+                    <strong className="text-dark-orange-2">{`${props.project.difficulty}/10`}
+                    </strong>  
+                    </span>
+                </Card.Text>
 */
